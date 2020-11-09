25b7337b82997aeac1f838c472c5e810cbf9066e
kingahmedino <ahmedino001>
2020-11-09 12:18:29 +0100

    Added change background image functionality in home screen

--- app/src/main/java/com/app/movieapp/screens/home/HomeFragment.kt
+++ app/src/main/java/com/app/movieapp/screens/home/HomeFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.app.movieapp.R
 import com.app.movieapp.adapters.CenterScaleLayoutManager
 import com.app.movieapp.adapters.MoviesAdapter
 import com.app.movieapp.databinding.FragmentHomeBinding
@@ -41,8 +44,24 @@ class HomeFragment : Fragment() {
                     CenterScaleLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
                 it.setHasFixedSize(true)
                 it.adapter = MoviesAdapter(requireContext(), movies)
+                changeBackgroundImage(it.layoutManager as LinearLayoutManager)
+            }
+        })
+
+        mBinding.homeRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                changeBackgroundImage(recyclerView.layoutManager as LinearLayoutManager)
             }
         })
     }
 
+    private fun changeBackgroundImage(layoutManager: LinearLayoutManager) {
+        val visiblePosition = layoutManager.findFirstCompletelyVisibleItemPosition()
+        if (visiblePosition > -1){
+            val currentView = layoutManager.findViewByPosition(visiblePosition)
+            val drawable = currentView!!.findViewById<ImageView>(R.id.movie_image_view).drawable
+            mBinding.bgndImage.setImageDrawable(drawable)
+        }
+    }
 }
