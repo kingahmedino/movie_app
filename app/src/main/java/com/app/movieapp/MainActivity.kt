5254d4995e2040b0b1109d37f4b6809d71ffcace
kingahmedino <ahmedino001>
2020-11-24 16:52:20 +0100

    Set flags for transparent status bar in Main Activity

--- app/src/main/java/com/app/movieapp/MainActivity.kt
+++ app/src/main/java/com/app/movieapp/MainActivity.kt
@@ -2,11 +2,13 @@ package com.app.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
     }
 }
