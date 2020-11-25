97a36f40cddda70cb69c98a973ca7dc84de1bce7
kingahmedino <ahmedino001>
2020-11-25 12:37:05 +0100

    Setup sample Movie Actors

--- app/src/main/java/com/app/movieapp/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/app/movieapp/screens/home/HomeViewModel.kt
@@ -8,26 +8,24 @@ import com.app.movieapp.models.Movie
 
 class HomeViewModel: ViewModel() {
     val movies = MutableLiveData<MutableList<Movie>>()
-    val actors = MutableLiveData<MutableList<Actor>>()
+    var actors = mutableListOf<Actor>()
 
-    fun setUpMoviesList(){
-        val moviesList = mutableListOf<Movie>()
-        moviesList.add(Movie("Joker", null, R.drawable.joker_1, null))
-        moviesList.add(Movie("The Hustle", null, R.drawable.hustle_2, null))
-        moviesList.add(Movie("Bad Boys for Life", null, R.drawable.badboys_1, null))
-        moviesList.add(Movie("The Old Guard", null, R.drawable.old_guard_2, null))
-        moviesList.add(Movie("Sonic The Hedgehog", null, R.drawable.sonic_1, null))
-
-        movies.value = moviesList
+    private fun setUpActorsList(){
+        actors.add(Actor("Joaquin Joker", null, R.drawable.joaquin))
+        actors.add(Actor("Will Smith", null, R.drawable.will))
+        actors.add(Actor("Zazie Beetz", null, R.drawable.zazie))
+        actors.add(Actor("Robert De Niro", null, R.drawable.roberto))
     }
 
-    fun setUpActorsList(){
-        val actorsList = mutableListOf<Actor>()
-        actorsList.add(Actor("Joaquin Joker", null, R.drawable.joaquin))
-        actorsList.add(Actor("Will Smith", null, R.drawable.will))
-        actorsList.add(Actor("Zazie Beetz", null, R.drawable.zazie))
-        actorsList.add(Actor("Robert De Niro", null, R.drawable.roberto))
+    fun setUpMoviesList(){
+        setUpActorsList()
+        val moviesList = mutableListOf<Movie>()
+        moviesList.add(Movie("Joker", null, R.drawable.joker_1, actors))
+        moviesList.add(Movie("The Hustle", null, R.drawable.hustle_2, actors))
+        moviesList.add(Movie("Bad Boys for Life", null, R.drawable.badboys_1, actors))
+        moviesList.add(Movie("The Old Guard", null, R.drawable.old_guard_2, actors))
+        moviesList.add(Movie("Sonic The Hedgehog", null, R.drawable.sonic_1, actors))
 
-        actors.value = actorsList
+        movies.value = moviesList
     }
 }
