8caa4f45c363fd4a5ff23b9cb6a11ee56f929d9c
kingahmedino <ahmedino001>
2020-12-01 10:17:32 +0100

    Updated movie and actor to get values from repository in HomeViewModel

--- app/src/main/java/com/app/movieapp/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/app/movieapp/screens/home/HomeViewModel.kt
@@ -1,11 +1,13 @@
 package com.app.movieapp.screens.home
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.app.movieapp.models.Actor
-import com.app.movieapp.models.Movie
+import com.app.movieapp.repository.MovieRepository
 
 class HomeViewModel: ViewModel() {
-    val movies = MutableLiveData<MutableList<Movie>>()
-    var actors = mutableListOf<Actor>()
+    val movies = MovieRepository.getMovies()
+
+    fun getMovieActors(movie_id: Int): List<Actor>{
+        return MovieRepository.getMovieCast(movie_id)
+    }
 }
